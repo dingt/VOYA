ac64e7de89dfdf413ad36d512b2a6e5ee53ec730
dingt <ding.t@husky.neu.edu>
2013-11-17 16:06:53 -0500

    php file location update

--- VOYA/SampleTest/LoginViewController.m
+++ VOYA/SampleTest/LoginViewController.m
@@ -57,7 +57,7 @@
             NSString *post =[[NSString alloc] initWithFormat:@"username=%@&password=%@",[UserName text],[Password text]];
             NSLog(@"PostData: %@",post);
             
-            NSURL *url=[NSURL URLWithString:@"http://localhost/xampp/jsonlogin.php"];
+            NSURL *url=[NSURL URLWithString:@"http://localhost/xampp/login_3.php"];
             
             NSData *postData = [post dataUsingEncoding:NSASCIIStringEncoding allowLossyConversion:YES];
             
