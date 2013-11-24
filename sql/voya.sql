8aa8dd791e0eb6dbf2571e92d661cf066d73f891
Man Yang <yang.ma@husky.neu.edu>
2013-11-24 16:38:34 -0500

    datab

--- sql/voya.sql
+++ sql/voya.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Nov 24, 2013 at 04:21 PM
+-- Generation Time: Nov 24, 2013 at 04:34 PM
 -- Server version: 5.1.41
 -- PHP Version: 5.3.1
 
@@ -22,6 +22,102 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `city`
+--
+
+CREATE TABLE IF NOT EXISTS `city` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `url` varchar(1000) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`) 
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `city`
+--
+
+INSERT INTO `city` (`id`, `name`, `location`, `url`) VALUES
+(1, 'boston', 'Massachusetts', ''),
+(2, 'brookline', 'Massachusetts', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `districts`
+--
+
+CREATE TABLE IF NOT EXISTS `districts` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `city_id` int(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `understand` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`),
+  KEY `city_id` (`city_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `districts`
+--
+
+INSERT INTO `districts` (`id`, `city_id`, `name`, `understand`) VALUES
+(1, 1, 'Allston and Brighton (Allston-Brighton)', 'Located west of Boston proper, these districts (especially Brighton) are primarily residential, and are home to many students and young professionals. Brighton is abutted by Boston College, which is the terminus of the Green Line''s B Branch. The border between the two is a subject of debate, so they are often considered to be one neighborhood by outsiders.'),
+(2, 1, 'Chinatown', 'Great Asian food, great herbalists and next to downtown and the theater district. 4th largest Chinatown in the United States.'),
+(3, 1, 'Downtown', 'This is the hub of tourist activity with Faneuil Hall, the Freedom Trail, Boston Public Garden, and Boston Common. It is also the center of city and state governments, businesses, and shopping.'),
+(4, 1, 'Financial District', 'Boston''s business and financial center, this area has plenty of restaurants, bars, and tourist attractions such as the New England Aquarium.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `play`
+--
+
+CREATE TABLE IF NOT EXISTS `play` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `tpy` varchar(255) DEFAULT NULL,
+  `city_id` int(255) NOT NULL,
+  `location` varchar(255) DEFAULT NULL,
+  `explane` text,
+  PRIMARY KEY (`id`),
+  KEY `city_id` (`city_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Dumping data for table `play`
+--
+
+INSERT INTO `play` (`id`, `tpy`, `city_id`, `location`, `explane`) VALUES
+(1, 'do', 1, 'Newbury Street', ' Eight blocks of high-end boutiques, hair salons, and galleries. Makes for a fabulous day of shopping and dining. The shops and restaurants tend to be expensive, but one needn''t spend money to enjoy the area; one of Newbury''s main attractions is simply people-watching. College students, urban professionals, tourists, and street performers all mix here. Newbury Street is accessible on the Green Line from the Arlington, Copley, and Hynes stations.'),
+(2, 'buy', 1, 'Newbury Street', 'This shopping street runs the length of the Back Bay neighborhood. Often called "the Rodeo Drive of the East," Newbury St is a wonderfully dense avenue colored by historic brownstones and lots of shops and restaurants. Extremely expensive near Boston Common, but gradually becoming more affordable as you move toward Massachusetts Avenue. One block north from Boylston St, which is similar but less so. Vehicular traffic can be very slow on Newbury St itself; take parallel streets unless you have time to see the sights from your car. Accessible on the Green Line from Arlington, Copley, and Hynes stations.');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `see`
+--
+
+CREATE TABLE IF NOT EXISTS `see` (
+  `id` int(100) NOT NULL AUTO_INCREMENT,
+  `city_id` int(255) NOT NULL,
+  `place_name` varchar(255) DEFAULT NULL,
+  `intro` text,
+  PRIMARY KEY (`id`),
+  KEY `city_id` (`city_id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Dumping data for table `see`
+--
+
+INSERT INTO `see` (`id`, `city_id`, `place_name`, `intro`) VALUES
+(1, 1, 'Boston Children''s Museum', ' 300 Congress St, ? +1 617 426-7336. 10AM-5PM daily (F until 9PM). The Boston Children''s Museum is a large, modern museum recently built out of an industrial building along the Boston waterfront and relatively close to the Tea Party museum and the ICA. The quickest way to get there using mass transit is probably by walking from South Station, which is a large station on the Red Line. It has a variety of interactive exhibits about a considerable number of topics, as well as a reproduction of a traditionally Japanese house called the Kyoto House. It is also regularly host to a travelling exhibit from somewhere else in the country, which does not incur an additional admissions charge. It''s suitable for children of ages ranging from newborn to about 9. One of the most interesting things about the museum, particularly for children, is a 3-story climbing structure that lets the kids climb up from the ground floor to the third floor, in lieu of elevator or stairs. They support fitness and environmental sustainability programs, and they even have a green roof. It''s worthwhile if you''re bringing young children to Boston. $12, Ages 2–15 $12, Age 1 free. edit'),
+(2, 1, 'USS Constitution Museum', 'Charlestown Navy Yard, ? +1 617 426-1812. Apr-Oct Tu-Su 10AM-6PM, Nov-Mar Th-Su 10AM-3:50PM. Tour famous Old Ironsides, enjoy all-ages hands-on exhibits on sailing skills and crafts. Freewill donation.');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `understand`
 --
 
@@ -31,7 +127,8 @@ CREATE TABLE IF NOT EXISTS `understand` (
   `visit_time` varchar(1000) DEFAULT NULL,
   `history` text,
   `today` text,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `city_id` (`city_id`)
 ) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
 
 --
